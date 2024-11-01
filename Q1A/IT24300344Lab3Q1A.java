5fe1e223276db4d01a5bf24f4cee791a1c89bed5
IT24300344 <it24300344@my.sliit.lk>
2024-11-01 15:52:51 +0530

    Add files via upload

---
+++ Q1A/IT24300344Lab3Q1A.java
@@ -0,0 +1,8 @@
+public class IT24300344Lab3Q1A {
+public static void main(String[] args) {
+double pricePerKg = 225;
+double quantity = 5;
+double totalAmount = pricePerKg * quantity;
+System.out.printf("The total amount to pay is: %.2f\n", totalAmount);
+}
+}
