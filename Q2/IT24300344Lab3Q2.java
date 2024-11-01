e916f0120c752f1344a50969f1497e8a9b535959
IT24300344 <it24300344@my.sliit.lk>
2024-11-01 16:56:50 +0530

    Add files via upload

---
+++ Q2/IT24300344Lab3Q2.java
@@ -0,0 +1,9 @@
+public class IT24300344Lab3Q2 {
+public static void main(String[] args) {
+
+ double totalSalary = 50000 + (5 * 1000);
+
+System.out.println("Total Salary = " + totalSalary);
+
+}
+}
